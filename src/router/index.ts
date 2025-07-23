de1e5bd1da2a561601fb91fca2f33f3bb52ecb38
bbrockbrown <brockbrown46@gmail.com>
2025-07-23 11:03:15 -0700

    add lastfm callback

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,13 @@ import express from "express";
 
 import authentication from "./authentication";
 import users from "./users";
+import lastfm from "./lastfm";
 
 const router = express.Router();
 
 export default(): express.Router => {
   authentication(router);
   users(router);
+  lastfm(router);
   return router;
 }
